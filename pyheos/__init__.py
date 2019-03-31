77f8ffdc29145fb574c53080990a764fa696572c
andrewsayre <6730289+andrewsayre@users.noreply.github.com>
2019-03-31 00:39:34 -0500

    Add CommandError to init

--- pyheos/__init__.py
+++ pyheos/__init__.py
@@ -5,9 +5,11 @@ from .dispatch import Dispatcher
 from .heos import Heos
 from .player import HeosNowPlayingMedia, HeosPlayer
 from .source import HeosSource, InputSource
+from .response import CommandError
 
 __all__ = [
     'const',
+    'CommandError',
     'Dispatcher',
     'Heos',
     'HeosPlayer',
