c5d9b364dac6e6ceddd6510f3eaf7ad1d3a6260f
Andrew Sayre <andrew@sayre.net>
2019-04-24 20:25:31 -0500

    Fix heartbeat timeout handling

--- pyheos/const.py
+++ pyheos/const.py
@@ -1,12 +1,12 @@
 """Define consts for the pyheos package."""
 
 __title__ = "pyheos"
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 CLI_PORT = 1255
 DEFAULT_TIMEOUT = 10.0
 DEFAULT_RECONNECT_DELAY = 10.0
-DEFAULT_HEART_BEAT = 60.0
+DEFAULT_HEART_BEAT = 10.0
 DEFAULT_STEP = 5
 
 STATE_CONNECTED = "connected"
